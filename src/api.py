57deb959028ef5983f73a14e918dc1cfa401081f
Thiago Froz <thifroz@gmail.com>
2026-06-10 13:11:10 -0400

    Integra Football Data API

--- src/api.py
+++ src/api.py
@@ -1,5 +1,24 @@
 import requests
 
+API_KEY = "94922ad863484c1ca58891dda9d1f7f7"
 
-def buscar_selecao(nome):
-    print(f"\nBuscando informações sobre {nome}...")
+
+def buscar_competicoes():
+    url = "https://api.football-data.org/v4/competitions"
+
+    headers = {
+        "X-Auth-Token": API_KEY
+    }
+
+    try:
+        resposta = requests.get(url, headers=headers)
+
+        if resposta.status_code == 200:
+            return resposta.json()
+
+        print(f"\nErro na API: {resposta.status_code}")
+        return None
+
+    except Exception as erro:
+        print(f"\nErro: {erro}")
+        return None
--- src/selecoes.py
+++ src/selecoes.py
@@ -1,2 +1,13 @@
-def pesquisar_selecao(nome):
-    pass
+from src.api import buscar_competicoes
+
+
+def pesquisar_selecao():
+    dados = buscar_competicoes()
+
+    if not dados:
+        return
+
+    print("\nCompetições encontradas:\n")
+
+    for competicao in dados["competitions"][:10]:
+        print(f"- {competicao['name']}")
